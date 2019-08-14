088d5ab1c7015de6dce20713af2569ff6a3b27cb
“yuriko0922” <yrk922@gmail.com>
2019-08-14 15:42:02 +0900

    正解不正解の精査

--- isannkuizuApp/ViewController.swift
+++ isannkuizuApp/ViewController.swift
@@ -12,21 +12,34 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var questionNum: UINavigationItem!
     
-    @IBOutlet var questions: UIView!
+    @IBOutlet var questions: [UIView]!
     
     
     @IBOutlet weak var answerbutton: UIStackView!
-    
+    //問題の数
     var countQuestion: Int = 0
-    
+    //正解か不正解かの精査
     var answerCheck: [Bool] = []
+    //問題の正解
+    let questionAnswer: [Int] = [1, 3, 2]
+    //現在の問題番号
+    var nowQuestion: Int = 1
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        //問題数
+        countQuestion = questions.count
+        //今何問目か表示させる
+        questionNum.title = "\(nowQuestion)問目"
     }
 
-
+   //回答を確認していきます
+    func answer(hisAnswer: Int) {
+        //
+    }
+    
+    
 }
 
