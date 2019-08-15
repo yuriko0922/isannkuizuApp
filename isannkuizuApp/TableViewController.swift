24169948758a9836c2b73eee4f80efe8a94f6154
“yuriko0922” <yrk922@gmail.com>
2019-08-15 20:01:11 +0900

    できたっぽいけど不満

--- isannkuizuApp/TableViewController.swift
+++ isannkuizuApp/TableViewController.swift
@@ -13,23 +13,23 @@ class TableViewController: UITableViewController {
     // 遷移元からもらう情報
     // 正解不正解
     var receiveResults: [Bool] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
-
-
+    
+    
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // 問題の数のセル表示
         return receiveResults.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "risuluts", for: indexPath)
-//
+        //
         if receiveResults[indexPath.row] {
             cell.textLabel?.text = "問題\(indexPath.row + 1)⭕️"
         } else {
@@ -37,9 +37,9 @@ class TableViewController: UITableViewController {
         }
         return cell
     }
-
-    }
     
+}
+
 
 
 
--- isannkuizuApp/ViewController.swift
+++ isannkuizuApp/ViewController.swift
@@ -27,24 +27,25 @@ class ViewController: UIViewController {
     
     // 問題文
     let monndai: [String] = [
-    "問１日本の世界遺産『富士山－信仰の対象と芸術の源泉』は、2013年に（ ）として世界遺産登録されました。\n1. 文化遺産\n2. 自然遺産\n3. 山岳遺産\n4. 伝統遺産",
-    "問2イタリア共和国の世界遺産『フィレンツェの歴史地区』のあるフィレンツェを中心に、17世紀に栄えた芸術運動は何でしょうか。\n1. シュルレアリスム\n2. アバンギャルド\n3. ルネサンス",
-    "問3 2016年のオリンピック開催地であるリオ・デ・ジャネイロで、ブラジル独立100周年を記念して作られたキリスト像が立つ場所として、正しいものはどれか。\n1. コパカバーナの山\n2. コルコバードの丘"
+        "問１日本の世界遺産『富士山－信仰の対象と芸術の源泉』は、2013年に（ ）として世界遺産登録されました。\n1. 文化遺産\n2. 自然遺産\n3. 山岳遺産\n4. 伝統遺産",
+        "問2イタリア共和国の世界遺産『フィレンツェの歴史地区』のあるフィレンツェを中心に、17世紀に栄えた芸術運動は何でしょうか。\n1. シュルレアリスム\n2. アバンギャルド\n3. ルネサンス",
+        "問3 2016年のオリンピック開催地であるリオ・デ・ジャネイロで、ブラジル独立100周年を記念して作られたキリスト像が立つ場所として、正しいものはどれか。\n1. コパカバーナの山\n2. コルコバードの丘"
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questions.text = monndai[nowQuestion]
+        questions.text = monndai[nowQuestion - 1]
     }
-
+    
     // 画面遷移した瞬間の処理
     override func viewWillDisappear(_ animated: Bool) {
         // タイトルを問題と表示させる処理
         questionNum.title = "問題"
     }
-   
+    
     // 結果の画面から問題の画面に戻ってきたときの処理
     override func viewWillAppear(_ animated: Bool) {
+        questions.text = monndai[0]
         // タイトルを何問目に直す
         questionNum.title = "\(countQuestion)問目"
     }
@@ -52,7 +53,7 @@ class ViewController: UIViewController {
     // 回答の精査してアラートを表示させるよ
     func checkAnswer(putAnswer: Int) {
         // 回答あってるかの精査
-        if putAnswer == questionAnswer[nowQuestion] {
+        if putAnswer == questionAnswer[nowQuestion - 1] {
             if nowQuestion < monndai.count {
                 // アラートの表示（正解）
                 showAlertOk(title: "正解！", message: "次に問題に進むよ")
@@ -64,12 +65,10 @@ class ViewController: UIViewController {
         } else {
             // 不正解の時
             // アラート
-            showAlertOk(title: "残念！", message: "もう一回挑戦する？")
+            showAlertBad(title: "残念！", message: "もう一回挑戦する？")
         }
-    
+        
     }
-    
-    
     
     // アラート（不正解！）
     func showAlertBad(title: String?, message: String?) {
@@ -95,7 +94,7 @@ class ViewController: UIViewController {
     func showAlertOk(title: String?, message: String) {
         // アラート作る！！！
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-         // アラートのアクション！！！
+        // アラートのアクション！！！
         let close = UIAlertAction(title: "OK", style: .default, handler: {(action: UIAlertAction) in
             // 正解を配列にIN
             self.answerCheck.append(true)
@@ -110,31 +109,30 @@ class ViewController: UIViewController {
     // ボタン減らす
     func bottunCat() {
         // ボタンの合計数から問題番号をいいてボタンを減らす
-        let bottunCatNum: Int = answerbutton.arrangedSubviews.count - nowQuestion - 1
+        let bottunCatNum: Int = answerbutton.arrangedSubviews.count - nowQuestion
         //
         answerbutton.arrangedSubviews[bottunCatNum].isHidden = true
     }
+    
     // 問題を１進める
     func nextQut() {
-        
-        // 問題番号１進める
-        nowQuestion += 1
+        // ボタン減らす
+        bottunCat()
         
         // 現在の問題番号が問題数を超えないようにする
         if nowQuestion >= monndai.count {
             // 結果画面に遷移させる
             performSegue(withIdentifier: "toResult", sender: nil)
             reset()
+        } else {
+            questions.text = monndai[nowQuestion]
+            // タイトルの変更
+            questionNum.title = "\(nowQuestion)問目"
         }
         
-        // ボタン減らす
-        bottunCat()
-
-        // 次の問題表示
-        questions.text = monndai[nowQuestion]
-        // タイトルの変更
-        questionNum.title = "\(nowQuestion)問目"
-
+        // 問題番号１進める
+        nowQuestion += 1
+        
     }
     
     // 遷移先に情報を渡す
@@ -158,11 +156,11 @@ class ViewController: UIViewController {
             bottun.isHidden = false
         }
         // 問題番号リセット
-        nowQuestion = 1
+        nowQuestion = 0
         // 正解不正解リセット
         answerCheck = []
     }
-   //ボタン紐ずけ
+    //ボタン紐ずけ
     
     @IBAction func answerButton(_ sender: UIButton) {
         // 回答を確認します
